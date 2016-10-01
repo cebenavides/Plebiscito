b304bfa5df43aaf95d92acb82625caa15de9306a
Carlos Benavides <cebenavides@uninorte.edu.co>
2016-10-01 10:58:14 -0500

    Arreglado el script que enlaza los dropdowns

--- js/custom.js
+++ js/custom.js
@@ -1,46 +1,27 @@
 $(document).ready(function() {
-    var selectDropdown;
-    var arreglo;
-
+    var arreglo = [];
     $('#dropdown_dep').change(function() {
+        
         var selectedValue = $(this).val();
-        selectDropdown = $("#dropdown_ciudad").empty().html(' ');
-        selectDropdown.append($("<option disabled selected></option>").attr("value",0).text("Ce que je sais"));
-         selectDropdown.append($("<option></option>").attr("value",2).text("Jorge es una persona 2"));
-        appendnow(1,"Quest-ce");
+        var selectDropdown = $("#dropdown_ciudad").empty().html(' ');
+        selectDropdown.append($("<option disabled selected></option>").attr("value",0).text("Escoja una opción"));
         jQuery.ajax({
             url : 'prueba.jsp',
             type: 'GET',
             data : { id: selectedValue },
+            dataType: 'JSON',
             success : function(data) {
-                var dataresponse = JSON.parse(data);
-                appendnow(2,"Quest-ce que je suis");
-                for (var i = 0; i < dataresponse.length; i++) {
-                    selectDropdown.append($("<option></option>").attr("value",3).text("Jorge es una persona 3"));
-                    //console.log(dataresponse[i].idciudad+"//"+dataresponse[i].nombre);
+                for (var i = 0; i < data.length; i++) {
+                    selectDropdown.append($("<option></option>").attr("value",parseInt(data[i].idciudad)).text(data[i].nombre));
                 } 
+                selectDropdown.trigger('contentChanged');   
             }
         });
-        //console.log(arreglo.length);
-        //update();
-        selectDropdown.trigger('contentChanged');        
+             
     });
 
   
     $('#dropdown_ciudad').on('contentChanged', function() {
         $(this).material_select();
     });
-
-    function appendnow(value,text){
-        console.log("aqui")
-        selectDropdown.append($("<option></option>").attr("value",value).text(text));
-    }
-
-    function update(){        
-            appendnow(2,"Quest-ce que je suis");
-                for (var i = 0; i < arreglo.length; i++) {
-                    appendnow(1,"Quest-ce");
-                    console.log(arreglo[i].idciudad+"//"+arreglo[i].nombre);
-                } 
-    }
 });
